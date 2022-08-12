0f30e32dea5e9b4a7615907d430f726b44e6fc55
Slava <s220160@gmail.com>
2022-08-13 00:26:57 +0600

    add Task001 projeckt

---
+++ Task001/Program.cs
@@ -0,0 +1,7 @@
+﻿using static System.Console;
+// Задача 0
+Clear(); //Очищает терминал
+Write("Введите число: "); //
+int number = Convert.ToInt32(ReadLine()); //Convert.ToInt32 можно заменить int.Parse
+int sqr = number * number;
+WriteLine($"Квадрат числа {number}={sqr}");
